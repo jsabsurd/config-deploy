aa3604060c6776efec0eaba9d894833499cbd6c0
johnny <joh.stauch@proton.me>
2025-01-02 13:45:14 +0100

    neofatch script

--- dwm/config.h
+++ dwm/config.h
@@ -12,7 +12,7 @@ static const int showsystray        = 1;        /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=15" };
-static const char dmenufont[]       = "monospace:size=9";
+static const char dmenufont[]       = "monospace:size=15";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
